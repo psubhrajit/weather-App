19350b302bcef13a346853e61a369780b7788bec
psubhrajit <psubhrajitdhoni@gmail.com>
2021-07-23 20:39:49 +0530

    Initial Commit

---
+++ config.js
@@ -0,0 +1,9 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "fb16f72e09b92e46ab0bc3a2d62af4a6"
+    }
+ }   
+// 3709f89e826c2838310e77a773533f2d
+
+module.exports = constants;
